ab3a5305a5f021f15428dcbb413bd4cb6b8598f7
lastprogrammers <paul@lastprogrammer.com>
2021-06-21 22:01:31 +0900

    commit Disassembly-HandsOn project

---
+++ Disassembly/HandsOn/HandsOn/HandsOn.cpp
@@ -0,0 +1,49 @@
+// HandsOn.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+void ConditionalStatement()
+{
+    int x = 5;
+
+    __asm {
+        cmp x, 5;
+        je case_equal;
+        jmp case_end;
+
+case_equal:
+        mov x, 10;
+        cmp x, 5;
+        ja case_above;
+        jmp case_end;
+
+case_above:
+        mov x, 1;
+        cmp x, 5;
+        jb case_below;
+        jmp case_end;
+
+case_below:
+        mov x, 0;
+    }
+
+case_end:
+    std::cout << x << std::endl;
+}
+
+int main()
+{
+    int x;
+
+    x = 0;
+    x++;
+    ++x;
+    _InterlockedIncrement((long*)&x);
+    _InterlockedDecrement((long*)&x);
+
+    printf("Hello World!\n");
+
+    ConditionalStatement();
+}
+
